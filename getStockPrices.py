86f47635f424175d72a7a7076fac93a1a4b47753
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-24 10:01:34 -0700

    working request

---
+++ getStockPrices.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+# url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL'
+url = 'https://finance.yahoo.com/quote/AAPL'
+response = requests.get(url, headers=headers)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title.text)
