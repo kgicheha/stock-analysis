22ededd289146a674b806a2a9e0b9d8e0ac59499
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-24 10:55:02 -0700

    working requests for stockWatchList items

--- getStockPrices.py
+++ getStockPrices.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 stockWatchList = [{
     "Apple": {
-       "Ticker" : "APPL",
+       "Ticker" : "AAPL",
        "Target Price": 150
     },
     "Microsoft": {
@@ -18,22 +18,28 @@ stockWatchList = [{
 #   pass in the ticker to the url
     # return the price of each item
 
+    # loops through watchlist and passes in stock to getSymbol function
 def stocksInWatchList():
     for stock in stockWatchList:
-        stockSymbol(stock)
+        getStockSymbol(stock)
 
-def stockSymbol(stock):
-    print(stock)
+    # gets stocks symbol from watchList array
+def getStockSymbol(stock):
+    for key,value in stock.items():
+        print("Getting Price for: ", key)
+        # print(value['Ticker'])
+        priceRequest(value['Ticker'])
+
+def priceRequest(ticker):
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+    url = f'https://finance.yahoo.com/quote/{ticker}'
+    response = requests.get(url, headers=headers)
+    # print(response.status_code)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})["value"]
+
+    print(price)
 
 stocksInWatchList()
-
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
-# url = 'https://finance.yahoo.com/quote/AAPL'
-# response = requests.get(url, headers=headers)
-# # print(response.status_code)
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-# price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})["value"]
-
-# print(price)
-
