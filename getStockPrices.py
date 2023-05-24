55a56657e3ab918fd86ba3e517350f03f5855006
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-24 11:58:30 -0700

    successfully creates captures financial results

--- getStockPrices.py
+++ getStockPrices.py
@@ -4,9 +4,6 @@ import csv
 from stockWatchList import stockWatchList
 
 stockFinancialResults =[]
-# loop through each item in the stock list object
-#   pass in the ticker to the url
-    # return the price of each item
 
     # loops through watchlist and passes in stock to getSymbol function
 def stocksInWatchList():
@@ -16,9 +13,10 @@ def stocksInWatchList():
     # gets stocks symbol from watchList array
 def getStockSymbol(stock):
     for key,value in stock.items():
-        priceRequest(key, value['Ticker'])
+        priceRequest(key, value['Ticker'], value['Target Price'] )
 
-def priceRequest(stockName, ticker):
+    # web scrap from yahoo to get prices
+def priceRequest(stockName, ticker, targetPrice):
 
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
     url = f'https://finance.yahoo.com/quote/{ticker}'
@@ -29,17 +27,23 @@ def priceRequest(stockName, ticker):
 
     print(f"Current Stock Price for {stockName} is {price}")
 
-    # stockFinancialResults.append({f'Stock Name': {stockName}, "Price": {price}"})
+    priceDifferenceCalculator(stockName, price, targetPrice)
     # createCsvFile(stockName, price)
 
+# create function to see how far the stock price is from the target price
+def priceDifferenceCalculator(stockName, price, targetPrice):
+    priceDifference = round(float(price) - targetPrice, 2)
+    stockFinancialResults.append({"Stock Name": stockName, "Current Price": price, "Target Price": targetPrice, "Price Difference" : priceDifference})
 
+
+    # gets information from stockFinancialResults array to create csv file
     # create csv file for the stocks
-# def createCsvFile(stockName, price):
-#     with open('stock_information.csv', mode ='w') as csvfile:
-#         fieldnames = ['Stock Name', 'Current Price']
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerow(stockName, price)
-
+def createCsvFile(stockName, price):
+    with open('stock_information.csv', mode ='w', newline='') as csvfile:
+        fieldnames = ['Stock Name', 'Current Price']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # writer.writeheader()
+        writer.writerows([stockName, price])
 
 stocksInWatchList()
+print(stockFinancialResults)
