a7bcafb5ddf390f2a9f3c1e6e7140ed431e33dec
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-25 10:55:48 -0700

    added stocks to watchlist

--- stockWatchList.py
+++ stockWatchList.py
@@ -6,6 +6,14 @@ stockWatchList = [{
     "Microsoft": {
        "Ticker" : "MSFT",
        "Target Price": 320.00
+    },
+    "Amazon.com,Inc": {
+    "Ticker" : "AMZN",
+    "Target Price" : 100.00
+    },
+    "Alphabet Inc.": {
+    "Ticker" : "GOOG",
+    "Target Price" : 115.00
     }
 
 }]
