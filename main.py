b3d38e7e11d28304dfb8c6e298bc9bafe99dc254
Kevin Gicheha <gichehakevin@gmail.com>
2023-09-24 00:31:23 -0700

    updated comments

--- main.py
+++ main.py
@@ -7,76 +7,75 @@ from datetime import date
 stockFinancialResults = []
 
 # loops through watchlist and passes in stock to getSymbol function
-
-# gets data from stockList.py file
 def stocksInList():
     for symbol in stockList:
         stockInfoRequest(symbol)
 
 # fetches data from yahoo finance
 def stockInfoRequest(ticker):
-    current_stock = yf.Ticker(ticker)
-    current_stock_info = current_stock.info
+    # try:
+        current_stock = yf.Ticker(ticker)
+        current_stock_info = current_stock.info
 
-    stock_name = current_stock_info["shortName"]
-    symbol = current_stock_info["symbol"]
-    sector = current_stock_info["sector"]
-    currency = current_stock_info["currency"]
-    open_price = current_stock_info["open"]
-    current_price = current_stock_info["currentPrice"]
-    previous_close_price = current_stock_info["previousClose"]
-    volume = current_stock_info["volume"]
+        stock_name = current_stock_info["shortName"]
+        symbol = current_stock_info["symbol"]
+        sector = current_stock_info["sector"]
+        currency = current_stock_info["currency"]
+        open_price = current_stock_info["open"]
+        current_price = current_stock_info["currentPrice"]
+        previous_close_price = current_stock_info["previousClose"]
+        volume = current_stock_info["volume"]
 
-    # gets dividend rate
-    try:
-        div_rate = current_stock_info["dividendRate"]
-    except KeyError:
-        div_rate = current_stock_info["trailingAnnualDividendRate"]
+        # gets dividend rate
+        try:
+            div_rate = current_stock_info["dividendRate"]
+        except KeyError:
+            div_rate = current_stock_info["trailingAnnualDividendRate"]
 
-    # gets dividend yield
-    try:
-        div_yield = round((current_stock_info["dividendYield"]) * 100, 2)
-    except KeyError:
-         div_yield = round((current_stock_info["trailingAnnualDividendYield"]) * 100, 2)
+        # gets dividend yield
+        try:
+            div_yield = round((current_stock_info["dividendYield"]) * 100, 2)
+        except KeyError:
+            div_yield = round((current_stock_info["trailingAnnualDividendYield"]) * 100, 2)
 
-    # gets current dividend date
-    try:
-        div_date = current_stock_info["exDividendDate"]
-        formatted_div_date = date.fromtimestamp(div_date)
-    except KeyError:
-        formatted_div_date = "N/A"
+        # gets current dividend date
+        try:
+            div_date = current_stock_info["exDividendDate"]
+            formatted_div_date = date.fromtimestamp(div_date)
+        except KeyError:
+            formatted_div_date = "N/A"
 
-    beta = round(current_stock_info["beta"], 2)
-    pe_ratio = round(current_stock_info["trailingPE"], 2)
-    eps = round(current_stock_info["trailingEps"],2)
+        beta = round(current_stock_info["beta"], 2)
+        pe_ratio = round(current_stock_info["trailingPE"], 2)
+        eps = round(current_stock_info["trailingEps"],2)
 
-    # gets earnings Quarterly Growth
-    try:
-        earnings_quarterly_growth = round((current_stock_info["earningsQuarterlyGrowth"]) * 100,2)
-    except KeyError:
-        earnings_quarterly_growth = "N/A"
+        # gets earnings Quarterly Growth
+        try:
+            earnings_quarterly_growth = round((current_stock_info["earningsQuarterlyGrowth"]) * 100,2)
+        except KeyError:
+            earnings_quarterly_growth = "N/A"
 
-    market_cap = current_stock_info["marketCap"]
-    # formats market cap
-    millnames = ['', ' Thousand', ' Million', ' Billion', ' Trillion']
-    millidx = max(0, min(len(millnames)-1,
-                         int(math.floor(0 if market_cap == 0 else math.log10(abs(market_cap))/3))))
-    formatted_market_cap = '{:.0f}{}'.format(
-        market_cap / 10**(3 * millidx), millnames[millidx])
+        market_cap = current_stock_info["marketCap"]
+        # formats market cap
+        millnames = ['', ' Thousand', ' Million', ' Billion', ' Trillion']
+        millidx = max(0, min(len(millnames)-1,
+                            int(math.floor(0 if market_cap == 0 else math.log10(abs(market_cap))/3))))
+        formatted_market_cap = '{:.0f}{}'.format(
+            market_cap / 10**(3 * millidx), millnames[millidx])
 
-    fifty_two_week_low = current_stock_info["fiftyTwoWeekLow"]
-    fifty_two_week_high = current_stock_info["fiftyTwoWeekHigh"]
-    year_target_price_est = current_stock_info["targetMeanPrice"]
+        fifty_two_week_low = current_stock_info["fiftyTwoWeekLow"]
+        fifty_two_week_high = current_stock_info["fiftyTwoWeekHigh"]
+        year_target_price_est = current_stock_info["targetMeanPrice"]
 
-    stock_history = current_stock.history(period="max")
-    highest_hist_price = round(max(stock_history["High"]), 2)
+        stock_history = current_stock.history(period="max")
+        highest_hist_price = round(max(stock_history["High"]), 2)
 
 
-    stockResultsCompile(stock_name, symbol, sector, currency, open_price,
-                        current_price, previous_close_price, volume, div_rate, div_yield,
-                        formatted_div_date, beta, pe_ratio, eps, earnings_quarterly_growth, formatted_market_cap,
-                        fifty_two_week_low, fifty_two_week_high, year_target_price_est, highest_hist_price
-                        )
+        stockResultsCompile(stock_name, symbol, sector, currency, open_price,
+                            current_price, previous_close_price, volume, div_rate, div_yield,
+                            formatted_div_date, beta, pe_ratio, eps, earnings_quarterly_growth, formatted_market_cap,
+                            fifty_two_week_low, fifty_two_week_high, year_target_price_est, highest_hist_price
+                            )
 
 
 #  function appends stock information to a results array
