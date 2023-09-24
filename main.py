1925a2278f1cbff849fd700aef8e17909eca8519
Kevin Gicheha <gichehakevin@gmail.com>
2023-09-23 22:13:26 -0700

    working stockResultsCompile function

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from datetime import date
 stockFinancialResults = []
 
 # loops through watchlist and passes in stock to getSymbol function
+
+
 def stocksInList():
     for stock in stockList:
         getStockInformation(stock)
@@ -37,52 +39,63 @@ def stockInfoRequest(stockName, ticker, targetPrice):
     div_date = current_stock_info["exDividendDate"]
     formatted_div_date = date.fromtimestamp(div_date)
 
-    beta = round(current_stock_info["beta"],2)
-    pe_ratio = round(current_stock_info["trailingPE"],2)
+    beta = round(current_stock_info["beta"], 2)
+    pe_ratio = round(current_stock_info["trailingPE"], 2)
 
     market_cap = current_stock_info["marketCap"]
     # formats market cap
-    millnames = ['',' Thousand',' Million',' Billion',' Trillion']
-    millidx = max(0,min(len(millnames)-1,
-                        int(math.floor(0 if market_cap == 0 else math.log10(abs(market_cap))/3))))
-    formatted_market_cap = '{:.0f}{}'.format(market_cap / 10**(3 * millidx), millnames[millidx])
+    millnames = ['', ' Thousand', ' Million', ' Billion', ' Trillion']
+    millidx = max(0, min(len(millnames)-1,
+                         int(math.floor(0 if market_cap == 0 else math.log10(abs(market_cap))/3))))
+    formatted_market_cap = '{:.0f}{}'.format(
+        market_cap / 10**(3 * millidx), millnames[millidx])
 
     fifty_two_week_low = current_stock_info["fiftyTwoWeekLow"]
     fifty_two_week_high = current_stock_info["fiftyTwoWeekHigh"]
     target_price = current_stock_info["targetMeanPrice"]
 
     stock_history = current_stock.history(period="max")
-    stock_highest_price = round(max(stock_history["High"]),2)
-    print(stock_highest_price)
+    highest_hist_price = round(max(stock_history["High"]), 2)
 
-    # stockResultsCompile(stockName, ticker, currentPrice, targetPrice,
-    #                     beta, peRatio, dividendYield, yearEstimatePrice, marketCap)
+
+    stockResultsCompile(stock_name, symbol, sector, currency, open_price,
+                        current_price, previous_close_price, volume, div_rate, div_yield,
+                        formatted_div_date, beta, pe_ratio, formatted_market_cap,
+                        fifty_two_week_low, fifty_two_week_high, target_price, highest_hist_price
+                        )
 
 
 #  function appends stock information to a results array
-def stockResultsCompile(stockName, ticker, currentPrice, targetPrice, beta, peRatio, dividendYield, yearEstimatePrice, marketCap):
+def stockResultsCompile(stock_name, symbol, sector, currency, open_price,
+                        current_price, previous_close_price, volume, div_rate, div_yield,
+                        formatted_div_date, beta, pe_ratio, formatted_market_cap,
+                        fifty_two_week_low, fifty_two_week_high, target_price, highest_hist_price):
 
-    currentPrice = round(float(currentPrice),2)
-    targetPrice = round(targetPrice, 2)
 
-    priceDifference = round(currentPrice - targetPrice, 2)
+    print("Higest_price", highest_hist_price)
+    print("Current_price", current_price)
+    entry_price = round(.75 * highest_hist_price,2)
+    print("Entry Price", entry_price)
 
-    if priceDifference > 0:
+
+    if current_price > entry_price:
         indicator = "Wait To Buy"
     else:
         indicator = "Buy Now"
 
-    stockFinancialResults.append({"Stock Name": stockName,
-                                  "Ticker": ticker,
-                                  "Beta": beta,
-                                  "PE Ratio": peRatio,
-                                  "Dividend Yield": dividendYield,
-                                  "1y Target Estimate": yearEstimatePrice,
-                                  "Current Price": currentPrice,
-                                  "Target Entry Price": targetPrice,
-                                  "Price Difference": priceDifference,
-                                  "Wait/Buy Now": indicator})
-    createCsvFile()
+    print("Indicator", indicator)
+
+    # stockFinancialResults.append({"Stock Name": stockName,
+    #                               "Ticker": ticker,
+    #                               "Beta": beta,
+    #                               "PE Ratio": peRatio,
+    #                               "Dividend Yield": dividendYield,
+    #                               "1y Target Estimate": yearEstimatePrice,
+    #                               "Current Price": currentPrice,
+    #                               "Target Entry Price": targetPrice,
+    #                               "Price Difference": priceDifference,
+    #                               "Wait/Buy Now": indicator})
+    # createCsvFile()
 
 
 # gets information from stockFinancialResults array to create csv file
